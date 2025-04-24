0c0e7a3f84e4a719f291b3dd9ca6ffd341090227
saqibmanan <saqibmanan6@gmail.com>
2025-04-24 19:25:32 +0530

    updating the Homepage.py as the main for homepage

--- Components_testing/HomePage.py
+++ Components_testing/HomePage.py
@@ -1,97 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import os
 from dotenv import load_dotenv
-import time
+from load_driver import load_driver
+from component_visibility_test import component_visibility_test
 
-load_dotenv()
+import test_components
+
 options = webdriver.ChromeOptions()
 # options.add_argument("--window-size=1920,1080")
 # options.add_argument("start-maximized")
 # options.add_experimental_option("detach", True)
-
-options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-driver = webdriver.Chrome(options=options)
-home_url = f"https://{os.getenv('HOME_URL_USERNAME')}:{os.getenv('HOME_URL_PASSWORD')}@{os.getenv('HOME_URL_4')}"
-driver.get(home_url)
-
-def check_component_visibility(component_name, selector, selector_type=By.XPATH, timeout=10):
-    try:
-
-        # Wait for the element to be visible
-        element = WebDriverWait(driver, timeout).until(
-            EC.visibility_of_element_located((selector_type, selector))
-        )
-
-        # Check if the element is displayed
-        is_visible = element.is_displayed()
-
-        if is_visible:
-            print(f"✅ Component '{component_name}' is visible on the page")
-        else:
-            print(f"❌ Component '{component_name}' is not visible on the page")
-
-        return is_visible
-
-    except TimeoutException:
-        print(f"❌ Component '{component_name}' not found within {timeout} seconds")
-        return False
-    except NoSuchElementException:
-        print(f"❌ Component '{component_name}' not found on the page")
-        return False
-    except Exception as e:
-        print(f"❌ Error checking component visibility: {e}")
-        return False
-    # finally:
-        # Always close the browser
-        # driver.quit()
+# driver = webdriver.Chrome(options=options)
+# home_url = f"https://{os.getenv('HOME_URL_USERNAME')}:{os.getenv('HOME_URL_PASSWORD')}@{os.getenv('HOME_URL_4')}"
 
 
-if __name__ == "__main__":
+# if __name__ == '__main__':
+    # Load driver
+driver = load_driver()
+dev_url = os.getenv('HOME_URL_DEV')
+driver.get(dev_url)
+component_visibility_test()
 
-    #Check if Main top left logo in header is visible using XPath
-    check_component_visibility(
-        component_name = 'logo',
-        selector="//img[@alt='IDS-DRR Logo']",
-        selector_type=By.XPATH
-        )
-
-    #Check if Home link in header is visible using XPath
-    check_component_visibility(
-        component_name='Home',
-        selector="/html/body/main/header/div/div[2]/div[1]/a[1]",
-        selector_type=By.XPATH
-        )
-
-    # Check if Analytics link in header is visible using XPath
-    check_component_visibility(
-        component_name='Analytics',
-        selector="/html/body/main/header/div/div[2]/div[1]/a[2]",
-        selector_type=By.XPATH
-    )
-
-    # Check if Datasets link in header is visible using XPath
-    check_component_visibility(
-        component_name='Datasets',
-        selector="/html/body/main/header/div/div[2]/div[1]/a[3]",
-        selector_type=By.XPATH
-    )
-
-    # Check if About Us link in header is visible using XPath
-    check_component_visibility(
-        component_name='About Us',
-        selector="/html/body/main/header/div/div[2]/div[1]/a[4]",
-        selector_type=By.XPATH
-    )
-    # # Check if Language Dropdown in header is visible using XPath
-    # check_component_visibility(
-    #     component_name='About Us',
-    #     selector="Select-module_Select__YceVe",
-    #     selector_type=By.CLASS_NAME
-    # )
 
