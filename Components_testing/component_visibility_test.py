d36f5c560048d899172bda3fb20d577d2d567fef
saqibmanan <saqibmanan6@gmail.com>
2025-05-01 13:52:06 +0530

    solving the issues with the script crashing issue in local and remote

--- Components_testing/component_visibility_test.py
+++ Components_testing/component_visibility_test.py
@@ -4,22 +4,21 @@ from selenium.webdriver.common.by import By
 from component_visibility_func import check_component_visibility
 
 def component_visibility_test(driver):
-    time.sleep(3)
-    #Check if Main top left logo in header is visible using XPath
+    # Check if Main top left logo in header is visible using XPath
     check_component_visibility(
         driver,
-        component_name = 'logo',
+        component_name='logo',
         selector="//img[@alt='IDS-DRR Logo']",
         selector_type=By.XPATH
-        )
+    )
 
-    #Check if Home link in header is visible using XPath
+    # Check if Home link in header is visible using XPath
     check_component_visibility(
         driver,
         component_name='Home',
         selector="/html/body/main/header/div/div[2]/div[1]/a[1]",
         selector_type=By.XPATH
-        )
+    )
 
     # Check if Analytics link in header is visible using XPath
     check_component_visibility(
@@ -51,13 +50,13 @@ def component_visibility_test(driver):
         selector="Select-module_Select__YceVe",
         selector_type=By.CLASS_NAME
     )
-    # time.sleep(3)
+
     # Check if Homepage image is visible using XPath
     check_component_visibility(
         driver,
         component_name='Homepage Image',
         selector="/html/body/main/main/div/section[1]/div/img[1]",
-        selector_type=By.CLASS_NAME
+        selector_type=By.XPATH
     )
 
     # Check if H2 text is visible using XPath
@@ -84,13 +83,11 @@ def component_visibility_test(driver):
         selector_type=By.XPATH
     )
 
-
     # Check for visibility of the analytics board links.
 
     # Find all elements with a same class
-    time.sleep(3)
     analytics_elements = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[2]/div[2]/div/div[2]/div")
-    for element in enumerate(analytics_elements):
+    for index, element in enumerate(analytics_elements):
         check_component_visibility(
             driver,
             component_name='Analytics dashboard links',
@@ -102,12 +99,11 @@ def component_visibility_test(driver):
         text = element.text
         print(f" Text: {text}")
 
-
     # Check for visibility of dataset catalogs heading and subheadings.
 
     # Find all elements with a same class
     dataset_catalog = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[3]/div[1]")
-    for element in enumerate(dataset_catalog):
+    for index, element in enumerate(dataset_catalog):
         check_component_visibility(
             driver,
             component_name='Data Catalogs heading and subheadings',
@@ -122,8 +118,8 @@ def component_visibility_test(driver):
     # Check for visibility of dataset catalogs links.
 
     # Find all elements with a same class
-    dataset_catalog_links = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[3]/div[2]")
-    for element in enumerate(dataset_catalog_links):
+    dataset_catalog = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[3]/div[2]")
+    for index, element in enumerate(dataset_catalog):
         check_component_visibility(
             driver,
             component_name='dataset catalogs links',
@@ -138,8 +134,8 @@ def component_visibility_test(driver):
     # Check for visibility of Resources heading and subheading using Xpath.
 
     # Find all elements with a same class
-    resource_heading = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[4]/div[1]")
-    for element in enumerate(resource_heading):
+    dataset_catalog = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[4]/div[1]")
+    for index, element in enumerate(dataset_catalog):
         check_component_visibility(
             driver,
             component_name='Resources heading and subheading',
@@ -154,8 +150,8 @@ def component_visibility_test(driver):
     # Check for visibility of Resources cards and metadata using Xpath.
 
     # Find all elements with a same class
-    resource_cards = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[4]/div[2]/div")
-    for element in enumerate(resource_cards):
+    dataset_catalog = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[4]/div[2]/div")
+    for index, element in enumerate(dataset_catalog):
         check_component_visibility(
             driver,
             component_name='cards and metadata',
@@ -214,20 +210,66 @@ def component_visibility_test(driver):
         # Optional: Get additional information about the element
         text = element.text
         print(f" Text: {text}")
-    # Check for visibility of Partners Section using Xpath.
+
+    # Check for visibility of Supporters Section using Xpath.
+
+    # Checking  visibility for Supported By Section using Xpath.
 
     # Find all elements with a same class
-    partners_section = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[6]")
-    for index, element in enumerate(partners_section):
+    supported_section = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[6]/div/div[1]")
+    for index, element in enumerate(supported_section):
         check_component_visibility(
-            component_name='Partners Section',
-            selector="/html/body/main/main/div/section[6]",
+            driver,
+            component_name='Supported By Section',
+            selector="/html/body/main/main/div/section[6]/div/div[1]",
             selector_type=By.XPATH
         )
+
         # Optional: Get additional information about the element
         text = element.text
         print(f" Text: {text}")
 
+    # checking the funder's logos
+    check_component_visibility(
+        driver,
+        component_name='The Rockefeller foundation logo',
+        selector="/html/body/main/main/div/section[6]/div/div[1]/div/img[1]",
+        selector_type=By.XPATH
+    )
+    check_component_visibility(
+        driver,
+        component_name='The PJMF logo',
+        selector="/html/body/main/main/div/section[6]/div/div[1]/div/img[2]",
+        selector_type=By.XPATH
+    )
 
-        print("in the end now")
-        return driver
+    # Check for visibility of In collaboration with Section using Xpath.
+
+    # Find all elements with a same class
+    collab_section = driver.find_elements(By.XPATH, "/html/body/main/main/div/section[6]/div/div[2]")
+    for index, element in enumerate(collab_section):
+        check_component_visibility(
+            driver,
+            component_name='Partners Section',
+            selector="/html/body/main/main/div/section[6]/div/div[2]",
+            selector_type=By.XPATH
+        )
+
+        # Optional: Get additional information about the element
+        text = element.text
+        print(f" Text: {text}")
+
+    # checking the Partner's logos
+    check_component_visibility(
+        driver,
+        component_name='ASDMA logo',
+        selector="/html/body/main/main/div/section[6]/div/div[2]/div/img[1]",
+        selector_type=By.XPATH
+    )
+    check_component_visibility(
+        driver,
+        component_name='HPSDMA logo',
+        selector="/html/body/main/main/div/section[6]/div/div[2]/div/img[2]",
+        selector_type=By.XPATH
+    )
+    return driver
