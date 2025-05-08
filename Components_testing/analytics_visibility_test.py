f16610330687bed11e9b01d28d0eccdfe39f4d5e
saqibmanan <saqibmanan6@gmail.com>
2025-05-08 13:36:26 +0530

    completing components visibility in Analytics page

--- Components_testing/analytics_visibility_test.py
+++ Components_testing/analytics_visibility_test.py
@@ -5,7 +5,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from component_visibility_func import check_component_visibility
 
-district_values = []
+map_district_values = []
+chart_district_values = []
+table_district_values = []
 
 def analytics_visibility_test(driver):
 
@@ -175,9 +177,9 @@ def analytics_visibility_test(driver):
         # print(f"Number of options in dropdown: {len(dropdown_options)}")
     #
         for index, element in enumerate(dropdown_options):
-            district_values.append(element.text)
+            map_district_values.append(element.text)
 
-    print(district_values)
+    print(map_district_values)
 
     select_district_section = driver.find_elements(By.XPATH, "//*[contains(@id, 'content-map')]/div/div[1]/div/div/div[1]")
     for index, element in enumerate(select_district_section):
@@ -213,6 +215,16 @@ def analytics_visibility_test(driver):
             selector_type=By.XPATH
         )
 
+    # moving to Chart View tab on Analytics page
+    try:
+        element = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/main/div/main/div/div[1]/button[2]/span"))            )
+        driver.find_element(By.XPATH, "/html/body/main/div/main/div/div[1]/button[2]/span").click()
+        print("✅ Chart View clicked")
+    except:
+        print("❌ Chart View not found")
+        return
+
     # Check for visibility of the Chart View Components using XPath
 
     # Find all elements with a same class
@@ -229,6 +241,137 @@ def analytics_visibility_test(driver):
     check_component_visibility(
         driver,
         component_name='Select district dropdown label',
-        selector="//*[contains(@id, 'content-map')]/div/div[1]/div/div[1]/div[1]/label",
+        selector="//*[contains(@id, 'content-chart')]/div/div[1]/div/div[1]/div[1]/label",
         selector_type=By.XPATH
     )
+
+    # Check for visibility of the Chart View Select District Options using XPath
+
+    # Find all elements with a same class
+    select_district_section = driver.find_element(By.XPATH, "/html/body/main/div/main/div/div[4]/div/div[1]/div/div[1]/div[2]/select")
+    if select_district_section.tag_name.lower() == 'select':
+        # Get all options in the dropdown
+        dropdown_options = select_district_section.find_elements(By.TAG_NAME, "option")
+        # print(f"Number of options in dropdown: {len(dropdown_options)}")
+        #
+        for index, element in enumerate(dropdown_options):
+            chart_district_values.append(element.text)
+
+    print(chart_district_values)
+
+    select_district_section = driver.find_elements(By.XPATH,
+                                                   "//*[contains(@id, 'content-chart')]/div/div[1]/div/div/div[1]")
+    for index, element in enumerate(select_district_section):
+        check_component_visibility(
+            driver,
+            component_name='Show District Options',
+            selector="//*[contains(@id, 'content-chart')]/div/div[1]/div/div/div[1]",
+            selector_type=By.XPATH
+        )
+
+    # Check for visibility of the Chart View Select Revenue Circle Options using XPath
+
+    # Find all elements with a same class
+    select_revenue_section = driver.find_elements(By.XPATH,
+                                                  "//*[contains(@id, 'content-chart')]/div/div[1]/div/div[2]")
+    for index, element in enumerate(select_revenue_section):
+        check_component_visibility(
+            driver,
+            component_name='Show Revenue Circle Options',
+            selector="//*[contains(@id, 'content-chart')]/div/div[1]/div/div[2]",
+            selector_type=By.XPATH
+        )
+
+    # Check for visibility of the Chart View Select Month Options using XPath
+
+    # Find all elements with a same class
+    select_month_section = driver.find_elements(By.XPATH, "//*[contains(@id, 'content-chart')]/div/div[1]/div/div/div[3]")
+    for index, element in enumerate(select_month_section):
+        check_component_visibility(
+            driver,
+            component_name='Show Month picker Options',
+            selector="//*[contains(@id, 'content-chart')]/div/div[1]/div/div/div[3]",
+            selector_type=By.XPATH
+        )
+
+
+    #Performaing the same for Table View.
+
+    # moving to Table View tab on Analytics page
+    try:
+        element = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/main/div/main/div/div[1]/button[3]/span"))            )
+        driver.find_element(By.XPATH, "/html/body/main/div/main/div/div[1]/button[3]/span").click()
+        print("✅ Table View clicked")
+    except:
+        print("❌ Table View not found")
+        return
+
+    # Check for visibility of the Table View Components using XPath
+
+    # Find all elements with a same class
+    map_view_section = driver.find_elements(By.XPATH, "//*[contains(@id, 'content-table')]")
+    for index, element in enumerate(map_view_section):
+        check_component_visibility(
+            driver,
+            component_name='All Table View components',
+            selector="//*[contains(@id, 'content-table')]",
+            selector_type=By.XPATH
+        )
+
+    # Check if the Select District dropdown label is visible using XPath
+    check_component_visibility(
+        driver,
+        component_name='Select district dropdown label',
+        selector="//*[contains(@id, 'content-table')]/div/div[1]/div/div/div[1]/div[1]/label",
+        selector_type=By.XPATH
+    )
+
+    # Check for visibility of the Chart View Select District Options using XPath
+
+    # Find all elements with a same class
+    select_district_section = driver.find_element(By.XPATH, "/html/body/main/div/main/div/div[3]/div/div[1]/div/div/div[1]/div[2]/select")
+    if select_district_section.tag_name.lower() == 'select':
+        # Get all options in the dropdown
+        dropdown_options = select_district_section.find_elements(By.TAG_NAME, "option")
+        # print(f"Number of options in dropdown: {len(dropdown_options)}")
+        #
+        for index, element in enumerate(dropdown_options):
+            table_district_values.append(element.text)
+
+    print(chart_district_values)
+
+    select_district_section = driver.find_elements(By.XPATH,
+                                                   "//*[contains(@id, 'content-table')]/div/div[1]/div/div/div[1]")
+    for index, element in enumerate(select_district_section):
+        check_component_visibility(
+            driver,
+            component_name='Show District Options',
+            selector="//*[contains(@id, 'content-table')]/div/div[1]/div/div/div[1]",
+            selector_type=By.XPATH
+        )
+
+    # Check for visibility of the Chart View Select Revenue Circle Options using XPath
+
+    # Find all elements with a same class
+    select_revenue_section = driver.find_elements(By.XPATH, "//*[contains(@id, 'content-table')]/div/div[1]/div/div[2]")
+    for index, element in enumerate(select_revenue_section):
+        check_component_visibility(
+            driver,
+            component_name='Show Revenue Circle Options',
+            selector="//*[contains(@id, 'content-table')]/div/div[1]/div/div[2]",
+            selector_type=By.XPATH
+        )
+
+    # Check for visibility of the Chart View Select Month Options using XPath
+
+    # Find all elements with a same class
+    select_month_section = driver.find_elements(By.XPATH, "//*[contains(@id, 'content-table')]/div/div[1]/div/div/div[3]")
+    for index, element in enumerate(select_month_section):
+        check_component_visibility(
+            driver,
+            component_name='Show Month picker Options',
+            selector="//*[contains(@id, 'content-table')]/div/div[1]/div/div/div[3]",
+            selector_type=By.XPATH
+        )
+
